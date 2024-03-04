61a9658f851c375cf569d9a6ed35d483c5036411
ANMOL <anmolgera01@gmail.com>
2024-03-04 17:02:23 +0900

    Setting up and getting event data from calendar

---
+++ app.py
@@ -0,0 +1,18 @@
+import calendar
+import webbrowser
+
+webbrowser.open('http://example.com')  
+try:
+    # f = open("https://calendar.google.com/calendar/u/0/r?tab=rc")
+    # print(f)
+    webbrowser.open('https://calendar.google.com/calendar/u/0/r?tab=rc')
+
+except:
+    print("There is some error")
+
+
+# process to follow
+    
+# authentication to access API
+
+# 
